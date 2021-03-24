e81bd2bb978be4cbd3610abbc8e009d50570acbb
Areeb0206 <masafvi48@gmail.com>
2021-03-24 10:44:18 +0530

    first commit

--- src/spa/Home/Section3/Section3.js
+++ src/spa/Home/Section3/Section3.js
@@ -7,13 +7,140 @@ export default class Section3 extends Component{
 render()
 {
 return(
-
+<div className="sec3">
   <div className="section3">
 
      <div className="in-sec3">
+            
+            <div className="left">
 
 
-  a
+                 <div className="left-in">
+
+                     <h1>Programs</h1>
+                     <h2>Fitness Programs</h2>
+                    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua </h3>
+                     <button>Appointment</button>
+
+                 </div>
+
+
+
+
+
+
+
+
+
+            </div>
+
+
+            <div className="right">
+
+
+
+                    <div className="right-one">
+
+
+                          <div className="right-one-left"></div>
+
+                          <div className="right-one-right">
+
+                              <h2>Body Building</h2>
+                              <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
+
+                          </div>
+
+                    </div>
+
+
+
+
+                    <div className="right-two">
+
+
+                          <div className="right-two-left"></div>
+
+                          <div className="right-two-right">
+
+                              <h2>Aerobic Classes</h2>
+                              <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
+
+                          </div>
+
+
+                    </div>
+
+
+
+
+
+                    <div className="right-three">
+
+                         <div className="right-three-left"></div>
+
+                          <div className="right-three-right">
+
+                              <h2>Weight Lifting</h2>
+                              <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
+
+                          </div>
+
+
+                    </div>
+
+
+
+
+                    <div className="right-four">
+
+
+
+                           <div className="right-four-left"></div>
+
+                          <div className="right-four-right">
+
+                              <h2>Yoga Classes</h2>
+                             <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
+
+                          </div>
+
+
+                     </div>
+
+
+
+
+                    <div className="right-five">
+
+
+                          <div className="right-five-left"></div>
+
+                          <div className="right-five-right">
+
+                              <h2>Cardio Training</h2>
+                             <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
+
+                          </div>
+
+
+
+                    </div>
+
+
+
+
+
+
+
+
+</div>
+
+
+
+            </div>
+
+
       </div>
 
           </div>
