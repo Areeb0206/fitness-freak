67389cad5ac88e9ae6bf4da036827aaf69a1f605
Areeb0206 <masafvi48@gmail.com>
2021-03-29 12:50:02 +0530

    first commit

--- src/spa/Bmi/Bmi.js
+++ src/spa/Bmi/Bmi.js
@@ -1,23 +1,104 @@
-import React, { Component } from 'react';
+import React from "react";
 
+import "./Bmi.scss";
 
+class Bmi extends React.Component {
+  state = {
+    result: "",
+    height: "",
+    weight: "",
+    bmi: "",
+    checkHealth: ""
+  };
+  calBmi = () => {
+    const { height, weight } = this.state;
+    console.log(height);
 
-export default class Bmi extends Component{
+    const calcBmi = (weight / (height / 100) ** 2).toFixed(2);
+    this.setState({
+      bmi: calcBmi
+    });
 
-render()
-{
-return(
-          <div className="Bmi">
-bmi
+    console.log(this.state.bmi);
+   
+    let resultDisplay =
+      this.state.bmi > 25
+        ? (<p>overwieght</p>)
+        : (<p>perfect</p>);
+    this.setState({
+      result: resultDisplay
+    });
+  };
+  clearAll = () => {
+    console.log("test");
+    this.setState({
+      height: "",
+      weight: "",
+      bmi: "",
+      result: ""
+    });
+  };
 
+  render() {
+    return (
+      <div className="bmi">
+        <div className="bmi-heading">
+          <h1> Calculate your <span className="b-m-i"> BMI</span></h1>
+          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua </h2>
           </div>
 
-	);
+        <div className="container">
+          {" "}
+          <div className="ui card">
+           
+               <div className="fields">
+                <div class="field">
+                  <label>Height: </label>
+                  <input
+                    type="number"
+                    placeholder="Height in cms"
+                    value={this.state.height}
+                    onChange={e => this.setState({ height: e.target.value })}
+                  />
+                </div>
+                <br />
+                <div className="field">
+                  <label>Weight: </label>
 
+                  <input
+                    type="number"
+                    placeholder="Weight in Kgs"
+                    value={this.state.weight}
+                    onChange={e => this.setState({ weight: e.target.value })}
+                  />
+                </div>
+
+                </div>
+            <div className="buttons">
+
+                  <button className="ui button" tabindex="0" onClick={this.calBmi}>
+                    Check
+                  </button>
+                  <button
+                    className="ui button"
+                    tabindex="1"
+                    onClick={this.clearAll}
+                  >
+                    Clear
+                  </button>
+
+                 </div> 
+            </div>
+            <div className="display-bmi">
+              <p className="bmi-display">{` Your BMI is ${this.state.bmi}`}</p>
+              <p className="result-display">{this.state.result}</p>
+            </div>
+    
+        </div>
+      </div>
+    );
+  }
 }
 
-}
 
-
-
-
+export default Bmi;
--- src/spa/Plans/Plans.js
+++ src/spa/Plans/Plans.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Plans.scss';
 
 
 
@@ -7,11 +8,102 @@ export default class Plans extends Component{
 render()
 {
 return(
-          <div className="Plans">
+	<div className="three-plans">
+          <div className="plans">
+               
+               <div className="in-plan">
 
-plans
+                   <div className="in-plan-heading">Choose Your Pricing Plans</div>
+
+                   <div className="all-plans">
+
+
+			                    <div className="left-plan">
+
+			                       <div className="left-heading">
+
+			                             <h1>Class Drop-in</h1>
+
+			                             <h2>$ 30.0</h2>
+
+			                             <h3>Quaterly</h3>
+
+
+
+
+			                       </div>
+
+
+			                       <div className="left-bottom">
+
+
+			                               <h1>Free riding</h1>
+			                               <h1>Unlimited Equipment</h1>
+			                               <h1>Weight Loss CLasses</h1>
+			                               <h1>No time restriction</h1>
+			                               <h1>Personal Trainer</h1>
+			             
+
+
+
+			                       </div>
+
+
+			                   </div>
+
+
+			                     <div className="mid-plan">
+
+
+			                            <div className="mid-heading">  <h1>Class Drop-in</h1>
+
+			                             <h2>$ 30.0</h2>
+
+			                             <h3>Quaterly</h3></div>
+
+
+			                       <div className="mid-bottom">     <h1>Free riding</h1>
+			                               <h1>Unlimited Equipment</h1>
+			                               <h1>Weight Loss CLasses</h1>
+			                               <h1>No time restriction</h1>
+			                               <h1>Personal Trainer</h1></div>
+
+			                     </div>
+
+
+			                    <div className="right-plan">
+
+
+			                        <div className="right-heading">
+			                             <h1>Class Drop-in</h1>
+
+			                             <h2>$ 30.0</h2>
+
+			                             <h3>Quaterly</h3>
+</div>
+
+
+			                       <div className="right-bottom">
+			                               <h1>Free riding</h1>
+			                               <h1>Unlimited Equipment</h1>
+			                               <h1>Weight Loss CLasses</h1>
+			                               <h1>No time restriction</h1>
+			                               <h1>Personal Trainer</h1>
+			             </div>
+
+
+
+			                    </div>
+
+			               </div>
+
+
+
+               </div>
+
+
           </div>
-
+</div>
 	);
 
 }
